00bee0e08cfdfce96b6de00b195c2b6b251b26dc
Mustafa <jawadimustafa7@gmail.com>
2026-07-05 18:43:34 +0500

    Refactor movie and TV player URL functions
    Updated return statements for movie and TV player URLs to remove comments and use string concatenation.

--- js/api.js
+++ js/api.js
@@ -1,9 +1,7 @@
 function moviePlayerUrl(id) {
-  // Correct VidLink movie format: https://vidlink.pro/embed/movie/{id}
   return CONFIG.PLAYER_MOVIE_URL + id;
 }
 
 function tvPlayerUrl(id, season = 1, episode = 1) {
-  // Correct VidLink TV format: https://vidlink.pro/embed/tv/{id}?s={season}&e={episode}
-  return `${CONFIG.PLAYER_TV_URL}${id}?s=${season}&e=${episode}`;
+  return CONFIG.PLAYER_TV_URL + id + "?s=" + season + "&e=" + episode;
 }
